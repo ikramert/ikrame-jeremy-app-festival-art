1bb8e10da12b046bca962c740339bbf7699fe9fd
ikramert <112107968+ikramert@users.noreply.github.com>
2024-03-17 23:30:37 -0400

    LP 1.3C

--- duverseau_jeremy_rata_ikrame_p1_582-424MO/js/script.js
+++ duverseau_jeremy_rata_ikrame_p1_582-424MO/js/script.js
@@ -1,6 +1,33 @@
 /// ------------- APP -------------
 
 // swiper-filters
-const swiperFilters = new Swiper ('.swiper-filters', {
-    direction: 'horizontal',
+const swiperFilters = new Swiper(".swiper-filters", {
+  direction: "horizontal",
+});
+
+//Swiper LP
+const swiper = new Swiper(".swiper-screenshots", {
+  slidesPerView: 1,
+  centeredSlides: true,
+  loop: true,
+
+  breakpoints: {
+    500: {
+      slidesPerView: 1,
+      centeredSlides: true,
+      loop: true,
+    },
+
+    1200: {
+      spaceBetween: 30,
+      slidesPerView: 3,
+      centeredSlides: true,
+      loop: true,
+    },
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
